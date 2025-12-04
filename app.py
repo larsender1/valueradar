945ab5bfd14ef9f4edddcc2fad628001dc5d7497
Lars Ender <larsender@yahoo.com>
2025-12-04 21:34:08 +0100

    Update dashboard

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import render_template, request, session
 
 app = Flask(__name__)
 
